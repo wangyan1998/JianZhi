037c8a39bc030894d84de55cffbb7b6d494b6b56
wangyan1998 <1011344314@qq.com>
2021-09-12 19:58:36 +0800

    剑指offer

---
+++ tool/ListNode.java
@@ -0,0 +1,9 @@
+package toolclass;
+
+public class ListNode {
+    public ListNode next;
+    public int val;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
